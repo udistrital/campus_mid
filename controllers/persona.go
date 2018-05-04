adf8f88f7f213bde417d81d07ed826476003cacc
damillano93 <damillano93@gmail.com>
2018-05-04 11:20:16 -0500

    Creacion de errores y alertas a la funcion GuardarPersona

--- controllers/persona.go
+++ controllers/persona.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/astaxie/beego"
 	"github.com/udistrital/campus_mid/models"
@@ -27,39 +26,87 @@ func (c *PersonaController) URLMapping() {
 // @Failure 403 body is empty
 // @router /GuardarPersona [post]
 func (c *PersonaController) GuardarPersona() {
+	// persona datos que entran a la funcion GuardarPersona
 	var persona models.PersonaCompleta
+
+	// alerta que retorna la funcion Guardar persona
+	var alerta models.Alert
+
+	//acumulado de alertas
+	var alertas string
+
+	//almacena resultado de la peticion POST /persona
+	var resultado models.Persona
+
+	//almacena resultado de la peticion POST /persona_estado_civil
+	var resultado2 models.PersonaEstadoCivil
+
+	//almacena resultado de la peticion POST /persona_genero
+	var resultado3 models.PersonaGenero
+
+	//modelo PersonaGenero
+	var genero models.PersonaGenero
+
+	//modelo  PersonaEstadoCivil
+	var estadoCivil models.PersonaEstadoCivil
+
+	//valida que el JSON de entrada sea correcto
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &persona); err == nil {
-		fmt.Println(persona)
-		var resultado models.Persona
-		err := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona", "POST", &resultado, persona)
-		fmt.Println("este es el resultado", resultado.Id)
-		fmt.Println("este es el error", err)
 
-		var estadoCivil models.PersonaEstadoCivil
-		estadoCivil.Persona = &resultado
-		estadoCivil.EstadoCivil = persona.EstadoCivil
+		//funcion que realiza  de la  peticion POST /persona
+		errPersona := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona", "POST", &resultado, persona)
 
-		var resultado2 interface{}
-		errEstadoCivil := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_estado_civil", "POST", &resultado2, estadoCivil)
-		fmt.Println("este es el resultado", resultado2)
-		fmt.Println("este es el error", errEstadoCivil)
+		if errPersona == nil && resultado.Id != 0 {
 
-		var genero models.PersonaGenero
-		genero.Persona = &resultado
-		genero.Genero = persona.Genero
+			alertas = alertas + " OK persona "
+			alerta.Type = "OK"
+			alerta.Code = "201"
 
-		var resultado3 interface{}
-		errGenero := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_genero", "POST", &resultado3, genero)
-		fmt.Println("este es el resultado", resultado3)
-		fmt.Println("este es el error", errGenero)
+			estadoCivil.Persona = &resultado
+			estadoCivil.EstadoCivil = persona.EstadoCivil
+
+			//funcion que realiza  de la  peticion POST /persona_estado_civil
+			errEstadoCivil := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_estado_civil", "POST", &resultado2, estadoCivil)
+			if errEstadoCivil != nil || resultado2.Id == 0 {
+				alertas = alertas + " ERROR persona_estado_civil "
+				alerta.Type = "error"
+				alerta.Code = "400"
+			} else {
+				alertas = alertas + " OK persona_estado_civil "
+			}
+
+			genero.Persona = &resultado
+			genero.Genero = persona.Genero
+
+			//funcion que realiza  de la  peticion POST /persona_genero
+			errGenero := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_genero", "POST", &resultado3, genero)
+			if errGenero != nil || resultado3.Id == 0 {
+				alertas = alertas + " ERROR persona_genero "
+				alerta.Type = "error"
+				alerta.Code = "400"
+			} else {
+				alertas = alertas + " OK persona_genero "
+			}
+
+			alerta.Body = alertas
+			c.Data["json"] = alerta
+			c.ServeJSON()
+		} else {
+			alerta.Type = "error"
+			alerta.Code = "400"
+			alerta.Body = " ERROR persona "
+			c.Data["json"] = alerta
+			c.ServeJSON()
+		}
 
 	} else {
-		fmt.Println(err)
+		alerta.Type = "error"
+		alerta.Code = "400"
+		alerta.Body = "ERROR formato incorrecto"
+		c.Data["json"] = alerta
+		c.ServeJSON()
 	}
 
-	fmt.Println("guardando persona")
-
-	//c.Data["json"] = m
 	c.ServeJSON()
 
 }
--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -9,10 +9,10 @@ func init() {
 
 	beego.GlobalControllerRouter["github.com/udistrital/campus_mid/controllers:PersonaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/campus_mid/controllers:PersonaController"],
 		beego.ControllerComments{
-			Method:           "GuardarPersona",
-			Router:           `/GuardarPersona`,
+			Method: "GuardarPersona",
+			Router: `/GuardarPersona`,
 			AllowHTTPMethods: []string{"post"},
-			MethodParams:     param.Make(),
-			Params:           nil})
+			MethodParams: param.Make(),
+			Params: nil})
 
 }
