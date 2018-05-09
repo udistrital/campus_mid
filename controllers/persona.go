81be7da8cb1ee70ccd08445b456ade2f1b715128
Mafe Avendaño <mafe.ud@gmail.com>
2018-05-09 10:46:56 -0500

    Se define función ActualizarPersona

--- controllers/persona.go
+++ controllers/persona.go
@@ -118,12 +118,14 @@ func (c *PersonaController) GuardarPersona() {
 func (c *PersonaController) ActualizarPersona() {
 	// persona datos que entran a la funcion ActualizarPersona
 	var persona map[string]interface{}
+	var personaGenero []map[string]interface{}
+	var personaEstadoCivil []map[string]interface{}
 	//reultado de la creacion de la persona
 	var resultado map[string]interface{}
 	// resultado de la adicion del estado civil
-	//var resultado2 map[string]interface{}
+	var resultado2 map[string]interface{}
 	// reultado de la adicion del genero
-	//var resultado3 map[string]interface{}
+	var resultado3 map[string]interface{}
 	// alerta que retorna la funcion Guardar persona
 	var alerta models.Alert
 	//acumulado de alertas
@@ -131,52 +133,50 @@ func (c *PersonaController) ActualizarPersona() {
 
 	//valida que el JSON de entrada sea correcto
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &persona); err == nil {
-		//id := strconv.Itoa(persona["Id"].(int))
-		id := fmt.Sprintf("%.f", persona["Id"].(float64))
 		//funcion que realiza  de la  peticion PUT /persona
-		errPersona := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona/"+id, "PUT", &resultado, persona)
-		fmt.Println(errPersona)
-		if errPersona == nil && resultado["Id"] != 0 {
+		errPersona := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona/"+fmt.Sprintf("%.f", persona["Id"].(float64)), "PUT", &resultado, persona)
+		if errPersona == nil && resultado["Type"] == "success" {
 
 			alertas = alertas + " OK persona "
 			alerta.Type = "OK"
-			alerta.Code = "201"
-			/*
-				var estadoCivil map[string]interface{}
-				estadoCivil = make(map[string]interface{})
-				estadoCivil["Persona"] = resultado
-				fmt.Println("estado", estadoCivil)
-				estadoCivil["EstadoCivil"] = persona["EstadoCivil"]
+			alerta.Code = "200"
 
-				//funcion que realiza  de la  peticion PUT /persona_estado_civil
-				errEstadoCivil := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_estado_civil", "POST", &resultado2, estadoCivil)
-				if errEstadoCivil != nil || resultado2["Id"] == 0 {
-
-					alertas = alertas + " ERROR persona_estado_civil "
-					alerta.Type = "error"
-					alerta.Code = "400"
-				} else {
-					alertas = alertas + " OK persona_estado_civil "
-				}
-
-				var genero map[string]interface{}
-				genero = make(map[string]interface{})
-				genero["Persona"] = resultado
-				genero["Genero"] = persona["Genero"]
-
-				//funcion que realiza  de la  peticion PUT /persona_genero
-				errGenero := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_genero", "POST", &resultado3, genero)
-				if errGenero != nil || resultado3["Id"] == 0 {
+			//obtener id persona_genero
+			if err := request.GetJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_genero/?query=persona:"+fmt.Sprintf("%.f", persona["Id"].(float64)), &personaGenero); err == nil {
+				fmt.Println(personaGenero[0]["Id"])
+				//actualizar genero
+				personaGenero[0]["Genero"] = persona["Genero"]
+				errGenero := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_genero/"+fmt.Sprintf("%.f", personaGenero[0]["Id"].(float64)), "PUT", &resultado2, personaGenero[0])
+				fmt.Println(resultado2)
+				if errGenero != nil {
 					alertas = alertas + " ERROR persona_genero "
 					alerta.Type = "error"
 					alerta.Code = "400"
 				} else {
 					alertas = alertas + " OK persona_genero "
 				}
+			}
 
-				alerta.Body = alertas
-				c.Data["json"] = alerta
-				c.ServeJSON()*/
+			//obtener id persona_estado_civil
+			if err := request.GetJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_estado_civil/?query=persona:"+fmt.Sprintf("%.f", persona["Id"].(float64)), &personaEstadoCivil); err == nil {
+				fmt.Println(personaEstadoCivil[0]["Id"])
+				//actualizar estado_civil
+				personaEstadoCivil[0]["EstadoCivil"] = persona["EstadoCivil"]
+				fmt.Println(personaEstadoCivil[0]["EstadoCivil"])
+				errEstadoCivil := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_estado_civil/"+fmt.Sprintf("%.f", personaEstadoCivil[0]["Id"].(float64)), "PUT", &resultado3, personaEstadoCivil[0])
+				fmt.Println(resultado3)
+				if errEstadoCivil != nil {
+					fmt.Println(errEstadoCivil)
+					alertas = alertas + " ERROR persona_estado_civil "
+					alerta.Type = "error"
+					alerta.Code = "400"
+				} else {
+					alertas = alertas + " OK persona_estado_civil "
+				}
+			}
+			alerta.Body = alertas
+			c.Data["json"] = alerta
+			c.ServeJSON()
 		} else {
 			alerta.Type = "error"
 			alerta.Code = "400"
