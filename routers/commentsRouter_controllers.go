61ade7afa2525c4f0ca94101afa63f944da94be4
Mafe Avendaño <mafe.ud@gmail.com>
2019-08-11 19:08:34 -0500

    Actualizando rutas swagger

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -286,4 +286,49 @@ func init() {
             Filters: nil,
             Params: nil})
 
+    beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"] = append(beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"],
+        beego.ControllerComments{
+            Method: "PostProduccionAcademica",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"] = append(beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"],
+        beego.ControllerComments{
+            Method: "PutProduccionAcademica",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"] = append(beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"],
+        beego.ControllerComments{
+            Method: "DeleteProduccionAcademica",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"] = append(beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"],
+        beego.ControllerComments{
+            Method: "GetProduccionAcademica",
+            Router: `/:persona`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"] = append(beego.GlobalControllerRouter["github.com/planesticud/campus_mid/controllers:ProduccionAcademicaController"],
+        beego.ControllerComments{
+            Method: "PutEstadoAutorProduccionAcademica",
+            Router: `/estado_autor_produccion/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
 }
