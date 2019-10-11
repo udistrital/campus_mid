33d182c8fe7e19c00cb66e0cd101eae13367f022
Mafe Avendaño <mafe.ud@gmail.com>
2019-10-11 15:35:26 -0500

    Ajustando rutas a udistrital

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 set -u
 set -o pipefail
 
-if [ -n "${PARAMETER_STORE:-}" ]; then
-  export CAMPUS_MID__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/campus_mid/db/username --output text --query Parameter.Value)"
-  export CAMPUS_MID__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/campus_mid/db/password --output text --query Parameter.Value)"
-fi
+# if [ -n "${PARAMETER_STORE:-}" ]; then
+#   export CAMPUS_MID__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/campus_mid/db/username --output text --query Parameter.Value)"
+#   export CAMPUS_MID__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/campus_mid/db/password --output text --query Parameter.Value)"
+# fi
 
 exec ./main "$@"
